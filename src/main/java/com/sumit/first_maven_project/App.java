3f49645339d9fbfff17631f2bcc80b11fae3bd0f
sumit <taversumit@gmail.com>
2020-04-26 21:50:36 +0530

    Commit 2

--- src/main/java/com/sumit/first_maven_project/App.java
+++ src/main/java/com/sumit/first_maven_project/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        //Adding comment
+        //Adding comment again!
 	    System.out.println( "Hello World!!!" );
     }
 }
